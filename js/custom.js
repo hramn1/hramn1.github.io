d02fe60b3d566c7cc8db55511db87b79d075ced0
Unknown <stmihailov@yandex.ru>
2018-02-04 23:10:47 +0300

    правки

--- js/custom.js
+++ js/custom.js
@@ -3,22 +3,34 @@
   var menuButton = document.querySelector(".btn--menu");
   var menuCanvas = document.querySelector(".main-menu__offcanvas");
   var feature = document.querySelectorAll(".feature__item");
-    menuButton.addEventListener('click', function(evt){
-    menuCanvas.classList.add('main-menu__offcanvas--show')
-    });
+  var menuClose = document.querySelector(".close-cross");
+  var menuDrop = document.querySelector(".main-menu__dropdown");
+  var menuShow = document.querySelector(".drop-menu");
+  var menuDropTop = 0;
+  menuDrop.addEventListener('mouseover', function(evt){
+      menuShow.style.background = 'red';
+  });
+  menuButton.addEventListener('click', function(evt){
+    menuCanvas.classList.add('main-menu__offcanvas--show');
+  });
+  menuClose.addEventListener('click', function(evt){
+    menuCanvas.classList.remove('main-menu__offcanvas--show');
+  });
+  window.addEventListener('keydown', function(evt){
+    if(evt.keyCode == 27){
+      if(menuCanvas.classList.contains('main-menu__offcanvas--show')){
+        menuCanvas.classList.remove('main-menu__offcanvas--show');
+      }
+    }
+  });
   window.addEventListener('scroll', function(evt){
-    console.log(document.documentElement.scrollHeight);
-    console.log(document.documentElement.clientHeight);
-    console.log(window.pageYOffset );
     var scrollPage = window.pageYOffset;
     var scrollHeightPage = document.documentElement.scrollHeight;
     if(scrollPage * 2.6  > scrollHeightPage){
-    for(var i = 0;i<feature.length;i++){
-    feature[i].classList.add('feature__item--show')
-  }
-}
-  })
-
-  }
-)()
+      for(var i = 0;i<feature.length;i++){
+        feature[i].classList.add('feature__item--show');
+      }
+    }
+  });
+})();
 
