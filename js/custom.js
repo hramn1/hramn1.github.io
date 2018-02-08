f4d0aeae322bff5ac833bac96dc2d8b8583eef1e
Unknown <stmihailov@yandex.ru>
2018-02-08 23:35:19 +0300

    правки

--- js/custom.js
+++ js/custom.js
@@ -7,9 +7,50 @@
   var menuDrop = document.querySelector(".main-menu__dropdown");
   var menuShow = document.querySelector(".drop-menu");
   var menuDropTop = 0;
-  menuDrop.addEventListener('mouseover', function(evt){
-      menuShow.style.background = 'red';
+  var menuItems = document.querySelectorAll('.drop-menu li');
+  var menuHeight = 10 + menuItems.length * 60;
+  var timer;
+  if (document.body.clientWidth <= 750) {
+  menuDrop.addEventListener('click', function(evt){
+     menuShow.classList.toggle('drop-menu__show');
+     if (!menuShow.classList.contains('drop-menu__show')) {
+        menuShow.style.height = 0;
+        clearTimeout(timer);
+        menuDropTop = 0;
+     }
+     else{
+       var showingMenu = function(){
+         if(menuDropTop < menuHeight){
+          menuDropTop = menuDropTop + 5;
+          menuShow.style.height = menuDropTop + 'px';
+          timer = setTimeout(showingMenu, 20);
+         }};
+         showingMenu();
+      }
   });
+}
+else{
+  menuDrop.addEventListener('click', function(evt){
+     menuHeight = menuItems.length * 35;
+     menuShow.classList.toggle('drop-menu__show');
+     if (!menuShow.classList.contains('drop-menu__show')) {
+        menuShow.style.height = 0;
+        menuShow.style.display = 'none';
+        clearTimeout(timer);
+        menuDropTop = 0;
+     }
+     else{
+       var showingMenu = function(){
+         if(menuDropTop < menuHeight){
+          menuDropTop = menuDropTop + 10;
+          menuShow.style.height = menuDropTop + 'px';
+          menuShow.style.display = 'block';
+          timer = setTimeout(showingMenu, 10);
+         }};
+         showingMenu();
+      }
+  });
+}
   menuButton.addEventListener('click', function(evt){
     menuCanvas.classList.add('main-menu__offcanvas--show');
   });
