99094b5aedaaad056eec471129cc5659f7f59155
Unknown <stmihailov@yandex.ru>
2018-05-03 01:46:13 +0300

    ghfdrb

--- js/curs.js
+++ js/curs.js
@@ -1,9 +1,11 @@
-'use strict'
+
 
 let stringArray = [
 'создать проект дома своей мечты?',
 'начать карьеру дизайнера интерьеров?'
 ]
+let CharTimeout = 150;
+let StoryTimeout = 2000;
 
 let sliderHeader = document.querySelectorAll('.header-slide');
 
@@ -22,3 +24,37 @@ function nextSlide(){
   }
   currentslide++
 }
+
+function startTicker(){
+	 massiveItemCount =  stringArray.length;
+	 currentStory     = -1;
+	 currentLength    = 0;
+	 spanPrint     = document.querySelector('.main-header__print');
+	runTheTicker();     
+}
+function runTheTicker(){
+	let myTimeout;  
+	// Переход к следующему элементу
+	if(currentLength == 0){
+		currentStory++;
+		currentStory      = currentStory % massiveItemCount;
+		 storySummary      = stringArray[currentStory].replace(/"/g,'-');      
+	}
+	// Располагаем текущий текст в анкор с печатанием
+	spanPrint.innerHTML = storySummary.substring(0,currentLength);
+	// Преобразуем длину для подстроки и определяем таймер
+	if(currentLength != storySummary.length){
+
+		currentLength++;
+		myTimeout = CharTimeout;
+		
+	} else {
+		currentLength = 0;
+		myTimeout = StoryTimeout;
+	}
+	// Повторяем цикл с учетом задержки
+	setTimeout("runTheTicker()", myTimeout);
+}
+
+
+startTicker();
