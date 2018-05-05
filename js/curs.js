a5d0794fc36b33241c9e1fae841e1fc2f60d67f9
Unknown <stmihailov@yandex.ru>
2018-05-06 00:24:13 +0300

    правки

---
+++ js/curs.js
@@ -0,0 +1,91 @@
+let videoStudent = document.querySelector('.graduates-inner__video video');
+let stringArray = [
+'создать проект дома своей мечты?',
+'начать карьеру дизайнера интерьеров?'
+]
+let CharTimeout = 150;
+let StoryTimeout = 2000;
+let sliderHeader = document.querySelectorAll('.header-slide');
+let currentslide = 0;
+let slideTime = setInterval(slideVideoLeft,7000);
+let slideReviews = document.querySelectorAll('.video-slide');
+let currentWidthSlide = 0;
+let leftBtnSlide = document.querySelector('.video-control__left');
+let rightBtnSlide = document.querySelector('.video-control__right');
+
+videoStudent.volume = 0.02;
+videoStudent.setAttribute('autoplay','autoplay');
+
+setInterval(nextSlide,4000);
+startTicker();
+
+leftBtnSlide.addEventListener('click', function(){
+  slideVideoRight();
+  clearInterval(slideTime);
+});
+rightBtnSlide.addEventListener('click', function(){
+  slideVideoLeft();
+  clearInterval(slideTime);
+});
+
+function nextSlide(){
+  if(currentslide > 7){
+    currentslide = 0;
+    sliderHeader[7].classList.remove('showing');
+  }
+  sliderHeader[currentslide].classList.add('showing');
+  if(currentslide>0){
+    sliderHeader[currentslide-1].classList.remove('showing');
+  }
+  currentslide++
+}
+
+function startTicker(){
+   massiveItemCount =  stringArray.length;
+   currentStory     = -1;
+   currentLength    = 0;
+   spanPrint     = document.querySelector('.main-header__print');
+  runTheTicker();     
+}
+function runTheTicker(){
+  let myTimeout;  
+  if(currentLength == 0){
+    currentStory++;
+    currentStory      = currentStory % massiveItemCount;
+     storySummary      = stringArray[currentStory].replace(/"/g,'-');      
+  }
+  spanPrint.innerHTML = storySummary.substring(0,currentLength);
+
+  if(currentLength != storySummary.length){
+    currentLength++;
+    myTimeout = CharTimeout;
+  } else {
+    currentLength = 0;
+    myTimeout = StoryTimeout;
+  }
+  setTimeout("runTheTicker()", myTimeout);
+}
+
+
+function slideVideoLeft() {
+  if(currentWidthSlide <= -900){
+    currentWidthSlide = 100
+  }
+  currentWidthSlide -= 100;
+  for (var i = 0; i < slideReviews.length; i++) {
+    slideReviews[i].style.transform = 'translateX(' + currentWidthSlide + '%)';
+    slideReviews[i].style.transition = '2000ms';
+  }
+
+}
+function slideVideoRight() {
+  if(currentWidthSlide >= 0){
+    currentWidthSlide = -100
+  }
+  currentWidthSlide += 100;
+  for (var i = 0; i < slideReviews.length; i++) {
+    slideReviews[i].style.transform = 'translateX(' + currentWidthSlide + '%)';
+    slideReviews[i].style.transition = '2000ms';
+  }
+}
+
