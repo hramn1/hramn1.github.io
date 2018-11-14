822a55a7e10d45c5a6c04292069d34c47aa5f08f
hramn1 <stmihailov@yandex.ru>
2018-11-14 19:38:20 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 10,
+    spaceBetween: 0,
+    slidesPerGroup: 10,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
